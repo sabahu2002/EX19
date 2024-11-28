aaacd642c38960f53ba7a757b86d32aadfa63a27
sabahu2002 <130846830+sabahu2002@users.noreply.github.com>
2024-11-28 20:59:58 +0200

    EX19

---
+++ EVENT2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EVENT2 : MonoBehaviour
+{
+    public float speed = -30f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(speed * Time.deltaTime, speed * Time.deltaTime, speed * Time.deltaTime));
+    }
+}
+
